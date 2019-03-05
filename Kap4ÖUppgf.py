54f3cae456c53fa3ac76bd30871f0f10db4a48cd
ABBelinyq <43879756+ABBelinyq@users.noreply.github.com>
2019-03-05 16:09:32 +0100

    2019-03-05, EM lektion.
    Gjort uppgifter, påväg på 5.1.

---
+++ Kap4ÖUppgf.py
@@ -0,0 +1,26 @@
+# deltagare = [" lina ", " gunilla ", " erik "]
+# for person in deltagare :
+# print(’Välkommen ’ + person . capitalize () + ’!’)
+
+
+# 4:1
+# mängd=range(1000000)
+# print (sum(mängd))
+
+#4:2
+# tal=range(1, 501, 2)
+# print(sum(tal))
+
+#  4:3
+# registrerade =[" Anna ", "Eva", " Erik ", "Lars", " Karl "]
+# avanmannälningar =[" Anna ", " Erik ", " Karl "]
+# for avanmannälningar in registrerade:
+#     registrerade.remove(avanmannälningar)
+# print (registrerade)
+# # 4:4
+# förnamn =[" Maria ", " Erik ", " Karl "]
+# efternamn =[" Svensson ", " Karlsson ", " Andersson "]
+# for fnamn in efternamn:
+#     for enamn in förnamn:
+#         print(fnamn, enamn)
+
---
+++ Kap5ÖUppgf.py
@@ -0,0 +1,8 @@
+AKön = input("Ange kön")
+AHfärg = input("Ange hårfärg")
+AÖfärg = input("Ange ögonfärg")
+Namn = ["Daniel Radcliffe", "Rupert Grunt", "Emma Watson", "Selena Gomez"]
+Kön = ["man", "man", "kvinna", "kvinna"]
+Hfärg = ["brun", "röd", "brun", "brun"]
+ÖFärg = ["brun", "blå", "brun", "brun"]
+
