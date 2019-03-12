1138591823a1cc6fe091649d3c873d322ec63c11
ABBelinyq <43879756+ABBelinyq@users.noreply.github.com>
2019-03-12 16:09:48 +0100

    fasd
    ads

--- Kap5ÖUppgf.py
+++ Kap5ÖUppgf.py
@@ -1,8 +1,31 @@
-AKön = input("Ange kön")
-AHfärg = input("Ange hårfärg")
-AÖfärg = input("Ange ögonfärg")
-Namn = ["Daniel Radcliffe", "Rupert Grunt", "Emma Watson", "Selena Gomez"]
-Kön = ["man", "man", "kvinna", "kvinna"]
-Hfärg = ["brun", "röd", "brun", "brun"]
-ÖFärg = ["brun", "blå", "brun", "brun"]
+# Namn = ["Daniel Radcliffe", "Rupert Grunt", "Emma Watson", "Selena Gomez"]
+# Kön = ["Man", "Man", "Kvinna", "Kvinna"]
+# Hfärg = ["Brun", "Röd", "Brun", "Brun"]
+# ÖFärg = ["Brun", "Blå", "Brun", "Brun"]
 
+
+# AKön = input("Ange kön ")
+# AHfärg = input("Ange hårfärg ")
+# AÖfärg = input("Ange ögonfärg ")
+
+# AKön = AKön.capitalize()
+# AHfärg = AHfärg.capitalize()
+# AÖfärg = AÖfärg.capitalize()
+
+# Lenname = 0 
+# antalnamn = len(Namn)-1
+# Rättnamn = []
+# while(Lenname<=antalnamn):
+#     if (AKön == Kön[Lenname]and AHfärg == Hfärg[Lenname] and AÖfärg == ÖFärg[Lenname]):
+#         Rättnamn.append(Namn[Lenname])
+#         Lenname+=1
+#     else:
+#         Lenname+=1
+# if(len(Rättnamn)!=0):
+#     print("Du liknar: ", Rättnamn)
+# else:
+#     print("Du liknar ingen känd kändis")
+
+
+Å = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,]
+SH = [14, 13, 12, 11.5, 11, 10.5, 10, 9.5, 9, 8.5, 8]
