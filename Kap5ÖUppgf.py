ee61871a05f9ca9f38198827cb1ee1fc6a9fc814
ABBelinyq <43879756+ABBelinyq@users.noreply.github.com>
2019-03-26 13:38:41 +0100

    noice

--- Kap5ÖUppgf.py
+++ Kap5ÖUppgf.py
@@ -27,5 +27,26 @@
 #     print("Du liknar ingen känd kändis")
 
 
-Å = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,]
-SH = [14, 13, 12, 11.5, 11, 10.5, 10, 9.5, 9, 8.5, 8]
+# A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,]
+# SH = [14, 13, 12, 11.5, 11, 11, 10.5, 10, 10, 10, 9.5, 9, 9, 9, 9, 8.5, 8]
+# print("Hur gammal är du")
+# IA = int(input())
+# name = (input("skriv ditt för och efternamn"))
+# if (IA >17):
+#     print("Du behöver sova 8h timmar.")
+# else:
+#     ageLen = IA - 1
+#     print("Hallå", name, "! Enligt Vårdguidens rekommendationer behöver individer i din ålder" , SH[ageLen] , "timmar sömn.")
+
+Länder = ['sverige', 'norge', "danmark", "finland", "island", "england", "nordirland", "skottland", "wales"]
+
+userland = (input("Vilket land"))
+scandinavia=Länder[0:4]
+GB = Länder[5:8]
+Lowerland = userland.lower()
+if Lowerland in scandinavia:
+    print(Lowerland , "tillhör norden")
+elif Lowerland in GB:
+    print(Lowerland , "tillhör GB")
+else:
+    print("Landet är varken brittiskt eller skandinaviskt")
